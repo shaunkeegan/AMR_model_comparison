d88bbb12dc24aaca95d6c993ca3d7f8ec1648390
Shaun Keegan <shaun.keegan@glasgow.ac.uk>
2020-08-12 16:04:49 +0100

    Complete no prophylaxis model set

--- SEIR_NoProphylaxis.R
+++ SEIR_NoProphylaxis.R
@@ -1,4 +1,4 @@
-# Model B: SIR (no prophylaxis)
+# Model C: SEIR (no prophylaxis)
 # 
 # Shaun Keegan (keega.s.p@gmail.com)
 # August 2020
@@ -17,32 +17,40 @@
 
 # Model Function
 
-model.b <- function(times, init, parms){
+model.c <- function(times, init, parms){
   
   S <- init[1]
-  Is <- init[2]
-  Ir <- init[3]
-  R <- init[4]
+  Es <- init[2]
+  Er <- init[3]
+  Is <- init[4]
+  Ir <- init[5]
+  R <- init[6]
   
   with(as.list(parms),{
     
     # population total
-    N <- S + Is + Ir + R
+    N <- S + Es + Er + Is + Ir + R
     
     # susceptibles
     dS.dt <- - beta.s * S * Is/N - beta.r * S * Ir/N + rho * R
     
+    # exposed with susceptible strain
+    dEs.dt <- beta.s * S * Is/N  - lambda * Es
+    
+    # exposed with resistant strain
+    dEr.dt <- beta.r * S * Ir/N  - lambda * Er
+    
     # infected with susceptible strain
-    dIs.dt <- beta.s * S * Is/N  - epsilon * Is - gamma * Is
+    dIs.dt <- lambda * Es  - epsilon * Is - gamma * Is
     
     # infected with resistant strain
-    dIr.dt <- beta.r * S * Ir/N  + epsilon * Is - gamma * Ir
+    dIr.dt <- lambda * Er  + epsilon * Is - gamma * Ir
     
     # recovereds
     dR.dt <- gamma * Is + gamma * Ir - rho * R
     
     # model output
-    dX <- c(dS.dt, dIs.dt, dIr.dt, dR.dt)
+    dX <- c(dS.dt, dEs.dt, dEr.dt, dIs.dt, dIr.dt, dR.dt)
     list(dX)
   })
 }
--- execute_models.R
+++ execute_models.R
@@ -50,3 +50,27 @@ lines(out[,4] ~ out[,1], col = "orange", lwd = 3)
 lines(out[,5] ~ out[,1], col = "pink", lwd = 3)
 legend("topright", c("Susceptible","Infected (susceptible strain)","Infected (resistant strain)", "Recovered"), col = c("darkgreen", "red", "orange", "pink"), lty = 1, lwd = 3)
 
+
+
+
+# model e
+
+x <- c(98,0,0,1,1,0)
+parms <- c(beta.s = 0.05,beta.r = 0.05, rho = 0.01, epsilon = 0.001, gamma = 0.01, lambda = 0.05)
+times <- seq(0, 2000, 1)
+
+
+out <- as.data.frame(ode(x, times, model.c, parms))
+
+head(out)
+tail(out)
+
+plot(out[,2] ~ out[,1], ylim = c(0,120), type = "l", lwd ="3", col = "darkgreen",
+     main = "SIR (no prophylaxis)", xlab = "Time", ylab = "N")
+lines(out[,3] ~ out[,1], col = "lightblue", lwd = 3)
+lines(out[,4] ~ out[,1], col = "blue", lwd = 3)
+lines(out[,5] ~ out[,1], col = "red", lwd = 3)
+lines(out[,6] ~ out[,1], col = "orange", lwd = 3)
+lines(out[,7] ~ out[,1], col = "pink", lwd = 3)
+legend("topright", c("Susceptible","Exposed (susceptible strain)","Exposed (resistant strain)","Infected (susceptible strain)","Infected (resistant strain)", "Recovered"), col = c("darkgreen","lightblue","blue", "red", "orange", "pink"), lty = 1, lwd = 3)
+
